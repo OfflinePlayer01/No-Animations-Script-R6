185cedb385b023e6b943df0a418fa8351ac7a752
OfflinePlayer01 <168975740+OfflinePlayer01@users.noreply.github.com>
2024-05-29 13:51:26 +0300

    No_Animations_Script_R6.lua

--- No_Animations_Script_R6.lua
+++ No_Animations_Script_R6.lua
@@ -8,13 +8,13 @@ for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants
     end
  
     game:GetService("StarterGui"):SetCore("SendNotification", { 
-        Title = "Script By 17Dream";
-        Text = "My Discord Name : _offlineplayer_";
+        Title = "〰 Script By 917Dream 〰";
+        Text = "〰 My Discord Name : _offlineplayer_ 〰";
         Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
     Duration = 5;
 
         game:GetService("StarterGui"):SetCore("SendNotification", { 
-        Title = "No Animations Script R6 Loaded";
-        Text = "Enjoy :)";
+        Title = "〰 No Animations Script R6 Loaded 〰";
+        Text = "〰 Enjoy 〰";
         Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
     Duration = 5;
